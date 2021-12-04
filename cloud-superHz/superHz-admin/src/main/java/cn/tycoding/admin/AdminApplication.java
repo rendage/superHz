3c6ede054f0ac24edccafa0b9de64924906481de
renxunzhen <7518202@qq.com>
2021-12-04 22:54:14 +0800

    init superHz

---
+++ cloud-superHz/superHz-admin/src/main/java/cn/tycoding/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package cn.tycoding.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 服务提供方 - admin模块
+ *
+ * @author tycoding
+ * @date 2019-05-18
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+        System.out.println("admin 启动成功");
+    }
+}
